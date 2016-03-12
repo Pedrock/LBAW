fe72c6a799af8d9fa6951206f30e99f1787e993f
Ricardo <rcerqueira94@gmail.com>
2016-03-12 21:49:58 +0000

    about_us & contact_us 0.1

---
+++ a3/contact_us.php
@@ -0,0 +1,18 @@
+<?php 
+$title = "Online Store - My Orders";
+$css = ["my_orders.css"];
+
+include("templates/header.php") ?>
+<div id="featured-products">
+	<span class="title">Contact Us</span>
+</div>
+<div class="well">
+	Contact us at
+	<ul>
+		<li>Ana Catarina Dias Amaral, up201303169@fe.up.pt</li>
+		<li>José Soares Rebelo, up201303964@fe.up.pt</li>
+		<li>Pedro Miguel Vieira da Câmara, up201304073@fe.up.pt</li>
+		<li>Ricardo Dantas Cerqueira, up201304000@fe.up.pt</li>
+	</ul>
+</div>
+<?php include("templates/footer.php") ?>
