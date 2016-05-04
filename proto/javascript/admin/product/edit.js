8b1ac2b6fc2d17999e30e9365a1ea249f28d7cca
Ricardo <rcerqueira94@gmail.com>
2016-05-05 00:09:15 +0100

    added confirmation to product edit

--- proto/javascript/admin/product/edit.js
+++ proto/javascript/admin/product/edit.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
-	$("#files_btn").on('click', function(event) {
+	$("#files_btn").on("click", function(event) {
 		event.stopPropagation();
 		event.preventDefault();
 		$("#files").click();
 	});
 
-	$("#btn-edit-product").on('click', function(event) {
+	$("#btn-edit-product").on("click", function(event) {
 		event.stopPropagation();
 		event.preventDefault();
 		$("#main_form").submit();
 	});
 
+	$("#edit").on('hidden.bs.modal', function(event) {
+		window.location.href = "list.php";
+	});
+
 	function alert_error(error) {
-		console.log('alert: ' + error)
+		console.log("alert: " + error)
 	}
 
 	function input_error(selector, error) {
-		console.log('input: ' + selector + " - " + error)
+		console.log("input: " + selector + " - " + error)
 	}
 
 	function complete(xhr) {
 		console.log(xhr.responseText);
 		if (Math.floor(xhr.status / 100) == 2) {
-			window.location.href = "list.php"; // TODO
+			//window.location.href = "list.php"; // TODO
+			$("#edit").modal("toggle");
 		} else if (xhr.responseJSON) {
 			if (xhr.responseJSON.errors) {
 				input_valid(".form-group");
@@ -37,30 +42,30 @@ $(document).ready(function() {
 		}
 	}
 
-	$(".href_del").on('click', function(event) {
+	$(".href_del").on("click", function(event) {
 		event.preventDefault();
 		event.stopPropagation();
-		$('#del').modal('toggle');
+		$("#del").modal("toggle");
 	});
 
-	$("#btn_delete").on('click', function(event) {
-		console.log('id:' + id);
+	$("#btn_delete").on("click", function(event) {
+		console.log("id:" + id);
 		$.ajax({
 			url: "../../../api/admin/product/delete.php",
 			type: "POST",
 			data: "id=" + id,
 			success: function(html) {
-				console.log('success' + html);
+				console.log("success" + html);
 				window.location.href="list.php";
 			},
 			error: function(xhr, textStatus, errorThrown) {
-				console.log('error');
-				//$(".status_message").html('Failed to rename');
+				console.log("error");
+				//$(".status_message").html("Failed to rename");
 			}
 		});
 	});
 
-	$("#main_form").on('submit', function(event) {
+	$("#main_form").on("submit", function(event) {
 		event.preventDefault();
 		$.ajax({
 			url: "../../../api/admin/product/edit.php",
@@ -73,7 +78,7 @@ $(document).ready(function() {
 		});
 	});
 
-	$("#files").on('change', function(event) {
+	$("#files").on("change", function(event) {
 		var files = event.target.files;
 		var files_len = event.target.files.length;
 
@@ -85,7 +90,7 @@ $(document).ready(function() {
 
 		for (var i = 0; i < files_len; i++) {
 			total_size += files[i].size;
-			fd.append('file[' + i + ']', files[i]);
+			fd.append("file[" + i + "]", files[i]);
 
 			var reader = new FileReader();
 
@@ -93,8 +98,8 @@ $(document).ready(function() {
 				reader.onload = function(e) {
 					var elem = $("<div class=\"product_img\"></div>");
 					$(".drop_zone").append(elem);
-					elem.css('background-image', 'url("' + e.target.result + '")');
-					//elem.css('opacity', '0.5');
+					elem.css("background-image", "url(\"" + e.target.result + "\")");
+					//elem.css("opacity", "0.5");
 					//elem.html(ii);
 				}
 			}(i));
@@ -111,20 +116,20 @@ $(document).ready(function() {
 				cache: false,
 				processData: false,
 				success: function(html) {
-					console.log('success: ' + html);
+					console.log("success: " + html);
 				},
 				xhr: function() {
 					var xhr = $.ajaxSettings.xhr();
 					xhr.upload.onprogress = function(e) {
 						if (e.lengthComputable) {
 							//var perc = parseInt(100 * e.loaded / e.total);
-							//$(".status_message").html('Uploading... ' + perc + '%');
+							//$(".status_message").html("Uploading... " + perc + "%");
 						}
 					};
 					return xhr;
 				},
 				error: function(xhr, textStatus, errorThrown) {
-					console.log('error' + textStatus + errorThrown);
+					console.log("error" + textStatus + errorThrown);
 				}
 			});
 		}
