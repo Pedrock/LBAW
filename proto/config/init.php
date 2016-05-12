39f4f7641b2cebb0ba50a47b833c3d2056c572ec
José Rebelo <soaresrebelo@gmail.com>
2016-05-12 15:25:46 +0100

    Update init.php
    Added missing smarty assign

--- proto/config/init.php
+++ proto/config/init.php
@@ -27,6 +27,7 @@
   $smarty->assign('SUCCESS_MESSAGES', $_SESSION['success_messages']);
   $smarty->assign('FORM_VALUES', $_SESSION['form_values']);
   $smarty->assign('USERNAME', $_SESSION['username']);
+  $smarty->assign('load_start', $load_start);  
   
   unset($_SESSION['success_messages']);
   unset($_SESSION['error_messages']);  
