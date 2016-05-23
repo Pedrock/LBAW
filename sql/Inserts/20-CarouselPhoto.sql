6f777df9410b2994577078089421fb9122f2d858
Pedrock <camara-pedro@hotmail.com>
2016-05-23 16:37:07 +0100

    SQL corrigido

---
+++ sql/Inserts/20-CarouselPhoto.sql
@@ -0,0 +1,3 @@
+TRUNCATE CarouselPhoto RESTART IDENTITY CASCADE;
+
+INSERT INTO CarouselPhoto(photo_order, location) VALUES (1, 'carrossel1.jpg'),(2, 'carrossel2.jpg'),(3,'carrossel3.jpg'),(4, 'carrossel4.jpg'),(5, 'carrossel5.jpg');
