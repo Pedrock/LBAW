1b0f9fa789bb4dbf19ca45d9075d26f2368b6fbc
Ricardo <rcerqueira94@gmail.com>
2016-06-03 16:50:07 +0100

    wip

---
+++ proto/pages/admin/user/user.php
@@ -0,0 +1,9 @@
+<?php
+	include_once('../../../config/init.php');
+	include_once($BASE_DIR . 'database/products.php');
+
+	include_once($BASE_DIR . 'lib/admin_check.php');
+
+
+	$smarty->display('admin/user/user.tpl');
+?>
